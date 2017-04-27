b56e03528494abf826ba6a626ebb8b6eab616af2
Angel Shiwakoti <shiwakoti.angel@gmail.com>
2017-04-27 13:16:06 -0400

    Added the .gitignore file. Added bash script to run the server/client.

---
+++ bash.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Compile the Java Files
+javac ChatMessage.java
+javac ChatClient.java
+javac ChatClientGUI.java
+javac ChatServer.java
+javac ChatServerGUI.java
+
+# Read the command from terminal
+echo -n "What do you want to run? Type SERVER to run server and CLIENT to run client. [ENTER]: "
+read runType
+
+# Check the command to run server or client
+if  [ "$runType" == "SERVER" ]; then
+	java ChatServerGUI
+	exit 1
+elif [ "$runType" == "CLIENT" ]; then
+	java ChatClientGUI
+else
+	echo -n "Wrong command. Please run the script again and enter SERVER or CLIENT."
+	exit 1
+fi
