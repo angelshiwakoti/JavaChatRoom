6bb5878e67c33fc1dc9ae2612018ef171c4774b1
Angel Shiwakoti <shiwakoti.angel@gmail.com>
2017-05-03 20:47:15 -0400

    Reduced Echo effect in Audio call

--- ChatServer.java
+++ ChatServer.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.*;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
@@ -30,7 +31,8 @@ public class ChatServer {
 					System.out.println("Waiting for clients to connect...");
 					while (true) {
 						Socket clientSocket = serverSocket.accept();
-						BufferedReader inFromClient = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+						//BufferedReader inFromClient = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+						DataInputStream inFromClient = new DataInputStream(clientSocket.getInputStream());
 						DataOutputStream outToClient = new DataOutputStream(clientSocket.getOutputStream());
 						clientProcessingPool.submit(new ClientTask(clientSocket, inFromClient, outToClient));
 					}
@@ -47,11 +49,11 @@ public class ChatServer {
 
 	private class ClientTask implements Runnable{
 		private final Socket clientSocket;
-		private BufferedReader inFromClient;
+		private DataInputStream inFromClient;
 		private DataOutputStream outToClient;
 		private String userNickName;
 
-		private ClientTask(Socket clientSocket, BufferedReader inFromClient, DataOutputStream outToClient) {
+		private ClientTask(Socket clientSocket, DataInputStream inFromClient, DataOutputStream outToClient) {
 			this.clientSocket = clientSocket;
 			this.inFromClient = inFromClient;
 			this.outToClient = outToClient;
@@ -63,33 +65,68 @@ public class ChatServer {
 		public void run(){
 			System.out.println("Got a client !");
 
-			while(true){
-				
-				String clientSentence = "";
-				try {
-					clientSentence = this.inFromClient.readLine();
-				} 
-				catch (IOException e) {
-					e.printStackTrace();
-				}
+			int bytesRead = 0;
+        	byte[] inBytes = new byte[1];
+        	while(bytesRead != -1)
+        	{
+            	try
+            	{
+            		bytesRead = this.inFromClient.read(inBytes, 0, inBytes.length);
+            	}
+            	catch (IOException e)
+            	{
 
-				if(this.userNickName == null){
-					this.userNickName = clientSentence.toUpperCase();
-				}
-				else{
-					clientSentence = this.userNickName + ":" + clientSentence;
-				}
+            	}
+            	if(bytesRead >= 0)
+            	{
 
-				try {
-					for(ClientTask task: getClients()){
-							task.outToClient.writeBytes(clientSentence + "\n");
+            		try {
+						for(ClientTask task: getClients()){
+							if(this.outToClient != task.outToClient){
+								try
+								{
+									task.outToClient.write(inBytes, 0, bytesRead);
+								}
+								catch (IOException e)
+								{
+
+								}
+							}
+						}
+					}
+					catch(Exception e){
+
 					}
 				} 
-				catch (IOException e) {
-					e.printStackTrace();
-				}
+            }
 
-			}	
+			// while(true){
+				
+			// 	String clientSentence = "";
+			// 	try {
+			// 		clientSentence = this.inFromClient.readLine();
+			// 	} 
+			// 	catch (IOException e) {
+			// 		e.printStackTrace();
+			// 	}
+
+			// 	if(this.userNickName == null){
+			// 		this.userNickName = clientSentence.toUpperCase();
+			// 	}
+			// 	else{
+			// 		clientSentence = this.userNickName + ":" + clientSentence;
+			// 	}
+
+			// 	try {
+			// 		for(ClientTask task: getClients()){
+			// 				task.outToClient.writeBytes(clientSentence + "\n");
+			// 		}
+			// 	} 
+			// 	catch (IOException e) {
+			// 		e.printStackTrace();
+			// 	}
+
+			// }	
 		}
 	}
 	
